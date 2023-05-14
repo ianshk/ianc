b800c75b7b90ec0510afc6330180c62b55a0c794
Ian Callaghan <50110358+ianshk@users.noreply.github.com>
2023-05-14 21:14:04 +0800

    Update SideBar.tsx

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Link from "next/link";
 
 export default function SideBar() {
   return (
@@ -20,7 +19,6 @@ export default function SideBar() {
             <span className="ml-4">Follow on LinkedIn</span>
           </a>
         </li>
-
         <li className="mt-4 flex">
           <a
             className="group flex text-base text-slate-500 transition hover:text-slate-700"
@@ -37,7 +35,30 @@ export default function SideBar() {
                 d="M12 2C6.475 2 2 6.588 2 12.253c0 4.537 2.862 8.369 6.838 9.727.5.09.687-.218.687-.487 0-.243-.013-1.05-.013-1.91C7 20.059 6.35 18.957 6.15 18.38c-.113-.295-.6-1.205-1.025-1.448-.35-.192-.85-.667-.013-.68.788-.012 1.35.744 1.538 1.051.9 1.551 2.338 1.116 2.912.846.088-.666.35-1.115.638-1.371-2.225-.256-4.55-1.14-4.55-5.062 0-1.115.387-2.038 1.025-2.756-.1-.256-.45-1.307.1-2.717 0 0 .837-.269 2.75 1.051.8-.23 1.65-.346 2.5-.346.85 0 1.7.115 2.5.346 1.912-1.333 2.75-1.05 2.75-1.05.55 1.409.2 2.46.1 2.716.637.718 1.025 1.628 1.025 2.756 0 3.934-2.337 4.806-4.562 5.062.362.32.675.936.675 1.897 0 1.371-.013 2.473-.013 2.82 0 .268.188.589.688.486a10.039 10.039 0 0 0 4.932-3.74A10.447 10.447 0 0 0 22 12.253C22 6.588 17.525 2 12 2Z"
               ></path>
             </svg>
-            <span className="ml-4">Github</span>
+            <span className="ml-4">Follow on Github</span>
+          </a>
+        </li>
+        <li className="mt-4 flex">
+          <a
+            className="group flex text-base text-slate-500 transition hover:text-slate-700"
+            href="htps://github.com"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-6 w-6 flex-none"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+              />
+            </svg>
+
+            <span className="ml-4">Email me</span>
           </a>
         </li>
       </ul>
