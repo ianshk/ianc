17ba528d4d84e4e6ae15f992def9ffc453bf7031
Ian Callaghan <50110358+ianshk@users.noreply.github.com>
2023-05-14 22:18:55 +0800

    Update IntroDetails.tsx

--- src/components/IntroDetails.tsx
+++ src/components/IntroDetails.tsx
@@ -17,8 +17,11 @@ export default function IntroDetails() {
           <SideBar />
         </div>
         <div className="max-w-md">
+          <span className="inline-flex items-center rounded-full bg-teal-50 px-2 py-1 text-sm font-medium text-teal-900 ring-1 ring-inset ring-teal-600/20 mb-2">
+            Hi there 👋
+          </span>
           <h1 className="text-3xl md:text-4xl font-bold tracking-tight text-slate-800 pb-4">
-            Hi, I'm Ian, a Front-end web developer based in Hong Kong
+            I'm Ian, a Front-end web developer based in Hong Kong
           </h1>
           <p>
             I'm a front-end React developer with a passion for simplifying
