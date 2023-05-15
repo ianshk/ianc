a8a0f173317a7f1098392852558c29b42ae6dc4b
Ian Callaghan <50110358+ianshk@users.noreply.github.com>
2023-05-15 23:22:41 +0800

    Add beam analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Analytics from '@/components/Analytics';
 import './globals.css';
 import { Inter } from 'next/font/google';
 
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
