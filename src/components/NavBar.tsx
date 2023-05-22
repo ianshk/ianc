d3c5bf393ecf6cb23a79ad9b387aaebef9438446
Ian Callaghan <50110358+ianshk@users.noreply.github.com>
2023-05-22 23:22:35 +0800

    Update NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ import DarkSwitch from './DarkSwitch';
 export default function NavBar() {
   return (
     <div className="flex flex-row justify-between">
-      <p className="text-1xl mt-4 font-extrabold text-slate-700 dark:text-slate-50 md:text-2xl">
+      <p className="mt-4 text-2xl font-extrabold text-slate-700 dark:text-slate-50 md:text-3xl">
         Ian Callaghan
       </p>
       <div className="mt-5">
