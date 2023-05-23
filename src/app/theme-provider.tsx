32396856f37b6b47464247b66f766ce54f737381
Ian Callaghan <50110358+ianshk@users.noreply.github.com>
2023-05-23 13:27:22 +0800

    default to dark on the server

--- src/app/theme-provider.tsx
+++ src/app/theme-provider.tsx
@@ -2,7 +2,11 @@
 import { ThemeProvider } from 'next-themes';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+  return (
+    <ThemeProvider attribute="class" defaultTheme="dark">
+      {children}
+    </ThemeProvider>
+  );
 };
 
 export default Providers;
