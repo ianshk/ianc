4d37ebb4c8710653c13dadaf13756ff10dfac2c7
Ian Callaghan <50110358+ianshk@users.noreply.github.com>
2023-05-26 00:33:28 +0800

    Easy on the eyes

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -10,62 +10,40 @@ export default function TechStack() {
       <div className="pb-2">
         <div className="block pb-2 text-base font-semibold">Languages</div>
         <div className="flex flex-row flex-wrap gap-3 pb-2">
-          <div className="rounded bg-emerald-200 px-4 py-2 text-sm text-slate-800 dark:bg-emerald-300">
-            JavaScript
-          </div>
-          <div className="rounded bg-emerald-200 px-4  py-2 text-sm text-slate-800 dark:bg-emerald-300">
-            TypeScript
-          </div>
+          <div className="tech-pill">JavaScript</div>
+          <div className="tech-pill">TypeScript</div>
         </div>
       </div>
       <div className="pb-2">
         <div className="block pb-2 text-base font-semibold">Front-end</div>
         <div className="flex flex-row flex-wrap gap-3 pb-2">
-          <div className="rounded bg-cyan-200 px-4 py-2 text-sm text-slate-800 dark:bg-cyan-300">React</div>
-          <div className="rounded bg-cyan-200 px-4 py-2 text-sm text-slate-800 dark:bg-cyan-300">Next.js</div>
-          <div className="rounded bg-cyan-200 px-4 py-2 text-sm text-slate-800 dark:bg-cyan-300">Remix</div>
-          <div className="rounded bg-cyan-200 px-4 py-2 text-sm text-slate-800 dark:bg-cyan-300">
-            Vue | Nuxt
-          </div>
-          <div className="rounded bg-cyan-200 px-4 py-2 text-sm text-slate-800 dark:bg-cyan-300">Astro</div>
-          <div className="rounded bg-cyan-200 px-4 py-2 text-sm text-slate-800 dark:bg-cyan-300">
-            Tailwind CSS
-          </div>
+          <div className="tech-pill">React</div>
+          <div className="tech-pill">Next.js</div>
+          <div className="tech-pill">Remix</div>
+          <div className="tech-pill">Vue | Nuxt</div>
+          <div className="tech-pill">Astro</div>
+          <div className="tech-pill">Tailwind CSS</div>
         </div>
       </div>
       <div className="pb-2">
-        <div className="block pb-2 text-base font-semibold">Back-end | Database | ORM</div>
+        <div className="block pb-1 text-base font-semibold">Back-end | Database | ORM</div>
         <div className="flex flex-row flex-wrap gap-3 pb-2">
-          <div className="rounded bg-lime-200 px-4 py-2 text-sm text-slate-800 dark:bg-lime-300">Nest.js</div>
-          <div className="rounded bg-lime-200 px-4 py-2 text-sm text-slate-800 dark:bg-lime-300">
-            Express.js
-          </div>
-          <div className="rounded bg-lime-200 px-4 py-2 text-sm text-slate-800 dark:bg-lime-300">Prisma</div>
-          <div className="rounded bg-lime-200 px-4 py-2 text-sm text-slate-800 dark:bg-lime-300">Drizzle</div>
-          <div className="rounded bg-lime-200 px-4 py-2 text-sm text-slate-800 dark:bg-lime-300">
-            Planetscale
-          </div>
-          <div className="rounded bg-lime-200 px-4 py-2 text-sm text-slate-800 dark:bg-lime-300">Stripe</div>
+          <div className="tech-pill">Nest.js</div>
+          <div className="tech-pill">Express.js</div>
+          <div className="tech-pill">Prisma</div>
+          <div className="tech-pill">Drizzle</div>
+          <div className="tech-pill">Planetscale</div>
+          <div className="tech-pill">Stripe</div>
         </div>
       </div>
       <div className="pb-2">
         <div className="block pb-2 text-base font-semibold">Tools | Hosting</div>
         <div className="flex flex-row flex-wrap gap-3 pb-2">
-          <div className="rounded bg-amber-200 px-4 py-2 text-sm text-slate-800 dark:dark:bg-amber-300">
-            Vercel
-          </div>
-          <div className="rounded bg-amber-200 px-4 py-2 text-sm text-slate-800 dark:dark:bg-amber-300">
-            AWS (SST)
-          </div>
-          <div className="rounded bg-amber-200 px-4 py-2 text-sm text-slate-800 dark:dark:bg-amber-300">
-            Railway
-          </div>
-          <div className="rounded bg-amber-200 px-4 py-2 text-sm text-slate-800 dark:dark:bg-amber-300">
-            Cloudflare
-          </div>
-          <div className="rounded bg-amber-200 px-4 py-2 text-sm text-slate-800 dark:dark:bg-amber-300">
-            Docker
-          </div>
+          <div className="tech-pill">Vercel</div>
+          <div className="tech-pill">AWS (SST)</div>
+          <div className="tech-pill">Railway</div>
+          <div className="tech-pill">Cloudflare</div>
+          <div className="tech-pill">Docker</div>
         </div>
       </div>
     </div>
