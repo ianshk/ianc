d3a6984b8e82a020f93dee3fca6b7a62d7c6ddaf
Ian Callaghan <50110358+ianshk@users.noreply.github.com>
2023-05-28 22:24:53 +0800

    Update Intro.tsx

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -26,13 +26,11 @@ export default function Intro() {
             </span>{' '}
             based in Hong Kong
           </h1>
-
           <p>
             I'm a React developer who enjoys simplifying complex problems and staying updated with the latest
             web technologies. My main focus is on creating responsive, efficient, and dynamic websites and
             applications.
           </p>
-
           <p>
             I specialize in using React and its frameworks like <a href="https://nextjs.org/"> Next.js</a> and{' '}
             <a href="https://remix.run/"> Remix</a>. Additionally, I love working with the{' '}
@@ -40,18 +38,15 @@ export default function Intro() {
             applications that provide a smooth user experience. I also have a keen eye for design and can
             create visually appealing interfaces that not only look great but also function flawlessly.
           </p>
-
           <p>
             I have the necessary skills to convert design files from platforms such as Figma, Sketch, and
             Adobe XD into React components that accurately represent the original design.
           </p>
-
           <p>
             Throughout my professional career, I have acquired expertise in both front-end and back-end
             frameworks. This has provided me with a strong understanding of REST and GraphQL, enabling
             seamless integration with backend servers for optimal functionality.
           </p>
-
           <p>
             I am eager to learn and grow, and I enjoy working on fresh and exciting projects. If you're
             interested in collaborating, please feel free to <a href="mailto:ianshk@gmail.com"> reach out</a>.
