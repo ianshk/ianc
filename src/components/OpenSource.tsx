25162e48f96919c728d5882a611c7742707810c7
Ian Callaghan <50110358+ianshk@users.noreply.github.com>
2023-05-28 19:53:34 +0800

    Update OpenSource.tsx

--- src/components/OpenSource.tsx
+++ src/components/OpenSource.tsx
@@ -1,6 +1,31 @@
 import React from 'react';
 import ProjectCard from './ProjectCard';
 
+const cardItems = [
+  {
+    title: 'Movie Time',
+    description:
+      'A Movie suggestion site made with Next 13 (React) and Tailwind CSS. Uses features of Next 13 such as server components and fetching API data from the server.',
+    link: 'https://github.com/ianshk/newmovies',
+  },
+  {
+    title: 'Personal website',
+    description: 'This site made with Next 13 (React) and Tailwind CSS.',
+    link: 'https://github.com/ianshk/ianc',
+  },
+  {
+    title: 'Varia',
+    description: 'Create a headless design system using React Aria, Tailwind CSS, and cva for variants.',
+    link: 'https://github.com/ianshk/varia',
+  },
+  {
+    title: 'MantineTW',
+    description:
+      'An example repro shoing how to use the Mantine component library with TailwindCSS for styling and utilities.',
+    link: 'https://github.com/ianshk/MantineTW',
+  },
+];
+
 export default function OpenSource() {
   return (
     <div className="pb-16">
@@ -9,26 +34,9 @@ export default function OpenSource() {
         Open Source Projects
       </h2>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-        <ProjectCard
-          title="Movie Time"
-          description="A Movie suggestion site made with Next 13 (React) and Tailwind CSS. Uses features of Next 13 such as server components and fetching API data from the server."
-          link="https://github.com/ianshk/newmovies"
-        />
-        <ProjectCard
-          title="Personal website"
-          description="This site made with Next 13 (React) and Tailwind CSS."
-          link="https://github.com/ianshk/ianc"
-        />
-        <ProjectCard
-          title="Varia"
-          description="Create a headless design system using React Aria, Tailwind CSS, and cva for variants."
-          link="https://github.com/ianshk/varia"
-        />
-        <ProjectCard
-          title="MantineTW"
-          description="An example repro shoing how to use the Mantine component library with TailwindCSS for styling and utilities."
-          link="https://github.com/ianshk/MantineTW"
-        />
+        {cardItems.map((item) => (
+          <ProjectCard title={item.title} description={item.description} link={item.link} />
+        ))}
       </div>
     </div>
   );
