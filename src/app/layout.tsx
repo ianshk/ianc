b47c8741032f2f21d73072c16e3fe5b620f7e668
Ian Callaghan <50110358+ianshk@users.noreply.github.com>
2023-06-04 20:21:39 +0800

    meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import Providers from './theme-provider';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'React web developer based in Hong Kong - Ian Callaghan',
-  description: 'Personal website for Ian Callaghan a Hong Kong React developer',
+  title: 'Front-end web developer based in Hong Kong - Ian Callaghan',
+  description: 'Personal website for Ian Callaghan a Hong Kong front-end developer',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
